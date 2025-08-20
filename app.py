22037b49ece31758e6f3ce8c81028e566ef144dc
fabiodantas <fabiodantas@localhost.localdomain>
2025-08-19 23:04:18 -0300

    Cadastro e listas funcionando

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #bibliotecas
 import os
+#variáveis globais:
+restaurantes = []
+
 
 #funções
+
+#Exibir o nome do programa
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -12,25 +17,57 @@ def exibir_nome_do_programa():
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
+#Exibir o menu de opções
 def exibir_opcoes():
     print('1. CADASTRAR RESTAURANTE.')
     print('2. LISTAR RESTAURANTES.')
     print('3. ATIVAR RESTAURANTE.')
     print('4. SAIR.')
 
-def opcao_invalida():
-    input('Opção inválida. Digite qualquer tecla para voltar.')
+#Exibir subtítulo e limpando a tela
+def exibir_subtitulo(texto):
+	os.system('clean')
+	exibir_nome_do_programa()
+	print(texto)
+	print(' ')
+
+
+#Voltar ao menu principal
+def voltar_ao_menu_principal():
+    input('Digite qualquer tecla para voltar para o menu principal')
     main()
 
+
+#Exibir mensagem de opção inválida
+def opcao_invalida():
+    input('Opção inválida.')
+    voltar_ao_menu_principal()
+
+#Cadastrar restaurante
+def cadastrar_restaurante():
+    exibir_subtitulo('| CADASTRO DE RESTAURANTES |')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
+    voltar_ao_menu_principal()
+
+#Listar restaurantes
+def listar_restaurantes():
+    exibir_subtitulo('|           LISTA DE RESTAURANTES              |')
+    for i, restaurante in enumerate(restaurantes, start=1):
+        print(f'{i}º- {restaurante}\n')
+    voltar_ao_menu_principal()
+
+#Escolher opção
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         os.system('clear')
 
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante.')
+            cadastrar_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurantes.')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurantes.')
         elif opcao_escolhida == 4:
@@ -39,9 +76,12 @@ def escolher_opcao():
             opcao_invalida()
     except:
         opcao_invalida()
-
+        
+#finalizando o app
 def finalizando():
-    print('finalizando o app...')
+    exibir_subtitulo('APLICAÇÃO FINALIZADA.')
+
+
 #code
 def main():
     os.system('clear')
